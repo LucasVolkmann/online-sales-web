20bfaaff62b2bd47084f21b3580534ac7c9508d9
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-01-24 18:36:21 -0300

    feat: create module login

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import './App.css';
-
-import { useState } from 'react';
-import styled from 'styled-components';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import LoginScreen from './modules/login';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <TestDivStyled isColorized={count > 5} className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </TestDivStyled>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <LoginScreen />;
 }
-
-interface IColorizedDiv {
-  isColorized?: boolean;
-}
-
-const TestDivStyled = styled.div<IColorizedDiv>`
-  border-radius: 2px;
-  border: 1px solid black;
-  background-color: ${(props) => (props.isColorized ? 'rgba(0, 80, 0, 0.3)' : 'none')};
-`;
 
 export default App;
---
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackgroundImage = styled.img`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+`;
