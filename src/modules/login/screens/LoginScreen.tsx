843aa7151fec2f7cebff3390a93da03cd38f7a7c
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-01-24 19:37:23 -0300

    feat: create login component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,19 @@
-import { BackgroundImage } from '../styles/loginScreen.styles';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.jpg" />;
+      <ContainerLogin>
+        <LimitedContainer>
+          <LogoImage src="./logo.png" />
+        </LimitedContainer>
+      </ContainerLogin>
     </div>
   );
 };
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -4,7 +4,36 @@ export const BackgroundImage = styled.img`
   position: absolute;
   left: 0px;
   top: 0px;
-  width: 100%;
+  width: calc(100% - 350px);
   height: 100vh;
   object-fit: cover;
+`;
+
+export const LogoImage = styled.img`
+  width: 150px;
+`;
+
+export const ContainerLogin = styled.div`
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 100%;
+  max-width: 350px;
+  height: 100vh;
+  background-color: rgba(240, 240, 240, 1);
+  border-left: 1px solid rgba(200, 200, 200, 1);
+  box-shadow: -1px 0px 3px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LimitedContainer = styled.div`
+  height: fit-content;
+  width: 80%;
+  border: 1px solid black;
+
+  display: flex;
+  justify-content: center;
 `;
