0f6a9af47d0c343f394e8a500f9857dbb9c9038b
Lucas Eduardo Volkmann <lucas.volkmann2001@gmail.com>
2024-02-13 14:44:39 -0300

    feat: finish InsertProduct unit tests

--- src/modules/products/screens/__tests__/InsertProductScreen.test.tsx
+++ src/modules/products/screens/__tests__/InsertProductScreen.test.tsx
@@ -1,11 +1,8 @@
-import { act, fireEvent, render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { mockInsertProduct } from '../../__mocks__/mockInsertProduct.mock';
 import { InsertProductScreenTestIdEnum } from '../../enum/InsertProductScreenTestIdEnum';
 import InsertProductScreen from '../InsertProductScreen';
-
-let currentValue = '';
-let currentType = '';
 
 jest.mock('../../../../shared/hooks/useInsertProduct', () => ({
   useInsertProduct: () => ({
@@ -17,17 +14,23 @@ jest.mock('../../../../shared/hooks/useInsertProduct', () => ({
       currentValue = event.target.value;
     },
     handleSelectChange: jest.fn(),
-    handleOnClick: jest.fn(),
+    handleOnClick: mockHandleOnClick,
   }),
 }));
+const mockHandleOnClick = jest.fn();
+let currentValue = '';
+let currentType = '';
+
 jest.mock('../../../categories/hooks/useCategory', () => ({
   useCategory: () => ({
     categories: [],
   }),
 }));
 jest.mock('react-router-dom', () => ({
-  useNavigate: () => jest.fn(),
+  useNavigate: () => mockNavigate,
 }));
+
+const mockNavigate = jest.fn();
 
 describe('Test [InsertProductScreen]', () => {
   it('should be render', () => {
@@ -46,9 +49,7 @@ describe('Test [InsertProductScreen]', () => {
 
     const input = getByTestId(InsertProductScreenTestIdEnum.NAME_INPUT);
 
-    act(() => {
-      fireEvent.change(input, { target: { value: 'mock name' } });
-    });
+    fireEvent.change(input, { target: { value: 'mock name' } });
 
     expect(currentValue).toEqual('mock name');
     expect(currentType).toEqual('name');
@@ -58,9 +59,7 @@ describe('Test [InsertProductScreen]', () => {
 
     const input = getByTestId(InsertProductScreenTestIdEnum.IMAGE_INPUT);
 
-    act(() => {
-      fireEvent.change(input, { target: { value: 'mock image' } });
-    });
+    fireEvent.change(input, { target: { value: 'mock image' } });
 
     expect(currentValue).toEqual('mock image');
     expect(currentType).toEqual('image');
@@ -70,11 +69,35 @@ describe('Test [InsertProductScreen]', () => {
 
     const input = getByTestId(InsertProductScreenTestIdEnum.PRICE_INPUT);
 
-    act(() => {
-      fireEvent.change(input, { target: { value: 12345 } });
-    });
+    fireEvent.change(input, { target: { value: 12345 } });
 
     expect(currentValue).toEqual('123.45');
     expect(currentType).toEqual('price');
+  });
+  it('should call [handleOnClick] function when click the [insert button]', () => {
+    const { getByTestId } = render(<InsertProductScreen />);
+
+    const button = getByTestId(InsertProductScreenTestIdEnum.INSERT_BUTTON);
+
+    fireEvent.click(button);
+
+    expect(mockHandleOnClick).toHaveBeenCalled();
+  });
+  it('should call [navigate] function when click the [cancel button]', () => {
+    const { getByTestId } = render(<InsertProductScreen />);
+
+    const button = getByTestId(InsertProductScreenTestIdEnum.CANCEL_BUTTON);
+
+    fireEvent.click(button);
+
+    expect(mockNavigate).toHaveBeenCalled();
+  });
+  it('should be defined as false [loading, disabled] when the insert button has been render', () => {
+    const { getByTestId } = render(<InsertProductScreen />);
+
+    expect(getByTestId(InsertProductScreenTestIdEnum.INSERT_BUTTON)).toHaveProperty(
+      'disabled',
+      false,
+    );
   });
 });
